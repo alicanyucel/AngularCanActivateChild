29e99f58bfc4fbea47dcaa96e4ef87c8be5b7f67
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2023-12-02 19:53:22 +0300

    bileşen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    HomeComponent,
+    LoginComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
